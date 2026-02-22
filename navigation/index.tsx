8266d93aa2fcb5ad7ae07edf6e127a2848c08a88
Johnathon47 <hei.johnathon.2@gmail.com>
2026-02-22 23:04:56 +0300

    feat: add animated splash screen at app launch

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -5,6 +5,7 @@ import SignIn from '@/screens/SignIn';
 import SignUp from '@/screens/SignUp';
 import Transaction from "@/screens/Transaction";
 import Wallet from "@/screens/Wallet";
+import SplashScreen from '@/screens/SplashScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Pressable, StyleSheet, Text } from 'react-native';
 
@@ -32,6 +33,11 @@ export default function AppNavigator() {
         }
       })}
     >
+      <Stack.Screen
+      name="Splash"
+      component={SplashScreen}
+      options={{ headerShown:false }}
+      />
       <Stack.Screen
         name="MainMenu"
         component={MainMenu}

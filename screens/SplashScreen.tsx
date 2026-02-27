88c9e429af4af91a9e3cc8ca33c4d0404336b4f6
Johnathon47 <hei.johnathon.2@gmail.com>
2026-02-28 00:09:53 +0300

    feat: add some animation in the splashScreen and dependance and add the MoreSugar font on splashScreen

--- screens/SplashScreen.tsx
+++ screens/SplashScreen.tsx
@@ -1,21 +1,50 @@
 import React, { useEffect } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Animated, { CSSAnimationKeyframes } from "react-native-reanimated";
+import { useFonts } from "expo-font";
+
+const march: CSSAnimationKeyframes = {
+  "0%": {
+    transform: [{ translateX: -80 }, { rotateZ: "-15deg" }],
+  },
+  "50%": {
+    transform: [{ translateX: 0 }, { rotateZ: "0deg" }],
+  },
+  "100%": {
+    transform: [{ translateX: 80 }, { rotateZ: "15deg" }],
+  },
+};
+
 
 const SplashScreen = ({ navigation }: any) => {
+  const [fontsLoaded] = useFonts({
+    MoreSugar: require("@/assets/fonts/MoreSugar-Thin.ttf"),
+  });
 
   useEffect(() => {
     const timer = setTimeout(() => {
       navigation.replace("MainMenu");
-    }, 2000);
+    }, 7000);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <View style={styles.container}>
-        <Image style={styles.logoWhite}
-        source={require('@/assets/images/LogoPF-white.png')} />
-      <Text style={styles.title}>Million Dollar</Text>
+        <Animated.Image 
+                source={require('@/assets/images/LogoPF-white.png')}
+                style={[
+                  styles.logoWhite,
+                  {
+                    animationName: march,
+                    animationDuration: "2.5s",
+                    animationIterationCount: "infinite",
+                    animationTimingFunction: "ease-in-out",
+                    animationDirection: "alternate",
+                  },
+                ]}
+                />
+      <Text style={styles.title}>Million Dollars</Text>
       <Text style={styles.subtitle}>Gérez vos finances intelligemment</Text>
     </View>
   );
@@ -33,11 +62,13 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: "bold",
     color: "white",
+    fontFamily: "MoreSugar",
   },
   subtitle: {
     fontSize: 16,
     color: "white",
     marginTop: 10,
+    fontFamily: "MoreSugar",
   },
   logoWhite: {
     width: 200,

3929b4d2780c7335b853847ca8e0bba4bb2f9b67
Katsatsamy <velonyainaemilie@gmail.com>
2026-02-28 04:12:25 +0300

    fix: delate color2 and colorUsestate

--- components/login/SignUp.tsx
+++ components/login/SignUp.tsx
@@ -14,6 +14,7 @@ import {
 
 export default function SignUp() {
   const navigation = useNavigation<any>();
+  const color1 = "#264653";
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -28,6 +29,8 @@ export default function SignUp() {
     useState(false);
   const [showSuccessAlert, setShowSuccessAlert] = useState(false);
   // const [mail, setMail] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [signedUp, setSignedUp] = useState(false);
   const [fontsLoaded] = useFonts({
     MoreSugar: require("@/assets/fonts/MoreSugar-Thin.ttf"),
   });

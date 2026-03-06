6d5cfd8edc70ae38789acc15f643ee3e9e2985d6
Katsatsamy <velonyainaemilie@gmail.com>
2026-03-06 05:02:53 +0300

    feat: label all page style is done

--- components/label/EditLabelScrren.tsx
+++ components/label/EditLabelScrren.tsx
@@ -1,15 +1,9 @@
 import { RootStackParamList } from "@/navigation";
 import { session } from "@/service/session";
+import { FontAwesome } from "@expo/vector-icons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useState } from "react";
-import {
-  Alert,
-  Pressable,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import { updateLabel } from "./labelService";
 
 // Utilisez le type global RootStackParamList
@@ -23,6 +17,8 @@ export default function EditLabelScreen({ route, navigation }: Props) {
   const [name, setName] = useState(label.name || "");
   const [color, setColor] = useState(label.color || "#00ff00");
   const [iconRef, setIconRef] = useState(label.iconRef || "");
+  const [updateLabelMess, setUpdateLabelMess] = useState(false);
+  const [showNameAlert, setshowNameAlert] = useState(false);
   const accountId = session.getAccount()?.id || "";
 
   const token = session.getToken() || "";
@@ -30,16 +26,38 @@ export default function EditLabelScreen({ route, navigation }: Props) {
   const handleUpdate = async () => {
     try {
       if (!label.id) {
-        Alert.alert("Erreur", "ID du label manquant");
+        console.log("Id is empty !");
         return;
       }
 
-      await updateLabel(accountId, label.id, name, color, token, iconRef);
-      Alert.alert("Succès", "Label modifié");
-      navigation.goBack();
+      const data = await updateLabel(
+        accountId,
+        label.id,
+        name.toUpperCase(),
+        color,
+        token,
+        iconRef,
+      );
+
+      if (data.name && data.color) {
+        setUpdateLabelMess(true);
+        setTimeout(() => {
+          setUpdateLabelMess(false);
+        }, 3000);
+        setTimeout(() => {
+          navigation.goBack();
+        }, 4000);
+      }
+
+      if (data.code === 400) {
+        setshowNameAlert(true);
+        setTimeout(() => {
+          setshowNameAlert(false);
+        }, 3000);
+      }
     } catch (error) {
       console.error(error);
-      Alert.alert("Erreur", "Impossible de modifier le label");
+      console.log("Label can't updated");
     }
   };
 
@@ -50,7 +68,7 @@ export default function EditLabelScreen({ route, navigation }: Props) {
           fontFamily: "MoreSugar ",
           textAlign: "center",
           fontSize: 40,
-          paddingBottom: 10,
+          paddingBottom: 50,
         }}
       >
         Update Label
@@ -61,6 +79,14 @@ export default function EditLabelScreen({ route, navigation }: Props) {
         onChangeText={setName}
         style={styles.input}
       />
+      {showNameAlert && (
+        <View style={styles.inputAlertContainer}>
+          <FontAwesome name="info-circle" size={15} color="red" />
+          <Text style={styles.inputAlertContainerText}>
+            Label with name = {name} is already exist.
+          </Text>
+        </View>
+      )}
 
       <TextInput
         placeholder="Color (#FF0000)"
@@ -80,7 +106,7 @@ export default function EditLabelScreen({ route, navigation }: Props) {
         style={{
           backgroundColor: color1,
           padding: 20,
-          marginTop: 20,
+          marginTop: 30,
           borderRadius: 15,
           width: "75%",
         }}
@@ -97,6 +123,23 @@ export default function EditLabelScreen({ route, navigation }: Props) {
           Update
         </Text>
       </Pressable>
+      {updateLabelMess && (
+        <Text
+          style={{
+            position: "absolute",
+            bottom: 80,
+            alignSelf: "center",
+            backgroundColor: "green",
+            padding: 20,
+            color: "white",
+            fontFamily: "MoreSugar",
+            fontSize: 20,
+            borderRadius: 15,
+          }}
+        >
+          Updated Succesfully
+        </Text>
+      )}
     </View>
   );
 }
@@ -112,10 +155,24 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: color1,
     padding: 15,
-    marginBottom: 20,
+    marginTop: 20,
     borderRadius: 10,
     fontFamily: "MoreSugar",
-    fontSize: 16,
+    fontSize: 20,
     width: "90%",
+  },
+  inputAlertContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 2,
+  },
+  inputAlertContainerText: {
+    color: "red",
+    fontFamily: "MoreSugar",
+    fontSize: 15,
+    marginLeft: 10,
+    textAlign: "center",
   },
 });

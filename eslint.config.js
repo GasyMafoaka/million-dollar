f8e25467d72fd4a3fb559023b3796366dc55846f
Danian Heyos <151181925+Phylosius@users.noreply.github.com>
2026-03-15 10:55:56 +0300

    feat: transaction screen from wallet
    * feat: transaction screen from wallet
    
    * chore: add Bearer Authorization to API requests
    
    
    * fix: remove mock data and use session account ID in WalletTransactionScreen
    
    * fix: transaction display after creation
    
    * chore: made session manager sync
    
    * chore: try to fix things
    
    * feat: implement offline persistence and refactor session service
    
    * fix: transaction persistence and setup unit tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,22 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
+const jest = require("eslint-plugin-jest");
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ["dist/*"],
   },
+  {
+    files: ["**/*.test.ts", "**/*.test.tsx", "jest.setup.js"],
+    languageOptions: {
+      globals: {
+        ...jest.environments.globals.jest,
+      },
+    },
+    plugins: {
+      jest,
+    },
+  }
 ]);
